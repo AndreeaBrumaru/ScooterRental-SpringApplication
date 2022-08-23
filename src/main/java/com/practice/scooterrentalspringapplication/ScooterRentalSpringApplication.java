79df2ca5e79240092e3862eaa4c509c224b84d18
AndreeaBrumaru <andreea.brumaru@metro.digital>
2022-08-23 09:28:35 +0300

    Initial commit

---
+++ src/main/java/com/practice/scooterrentalspringapplication/ScooterRentalSpringApplication.java
@@ -0,0 +1,13 @@
+package com.practice.scooterrentalspringapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScooterRentalSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScooterRentalSpringApplication.class, args);
+	}
+
+}
